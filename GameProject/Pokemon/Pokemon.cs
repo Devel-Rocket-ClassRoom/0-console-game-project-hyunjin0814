e370ca73036e1bd2261568d8be57e2fee2a46aa5
hyunjin0814 <zse846@gmail.com>
2026-03-19 09:57:27 +0900

    저장공간 변경

---
+++ GameProject/Pokemon/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Framework.Engine;
+
+public class Pokemon : GameObject
+{
+    public Pokemon(Scene scene) : base(scene)
+    {
+
+    }
+    public override void Draw(ScreenBuffer buffer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Update(float deltaTime)
+    {
+        throw new NotImplementedException();
+    }
+}
