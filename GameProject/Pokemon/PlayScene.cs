30fbe737c891070d751d2539dd797f6c294c096c
hyunjin0814 <zse846@gmail.com>
2026-03-19 11:17:01 +0900

    캐릭터 이동 기능 구현
    키보드 4방향으로 이동 가능

--- GameProject/Pokemon/PlayScene.cs
+++ GameProject/Pokemon/PlayScene.cs
@@ -4,11 +4,15 @@ using Framework.Engine;
 public class PlayScene : Scene
 {
     private Wall wall;
+    private Player player;
 
     public override void Load()
     {
         wall = new Wall(this);
         AddGameObject(wall);
+
+        player = new Player(this, 29, 14);
+        AddGameObject(player);
     }
 
     public override void Unload()
--- GameProject/Pokemon/Player.cs
+++ GameProject/Pokemon/Player.cs
@@ -1,9 +1,12 @@
-﻿using System;
-using Framework.Engine;
+﻿using Framework.Engine;
+using System;
+using System.Xml.Linq;
 
 public class Player : GameObject
 {
     private (int X, int Y) _position;
+
+    public (int X, int Y) Position => _position;
 
     public Player(Scene scene, int startX, int startY) : base(scene)
     {
@@ -12,13 +15,34 @@ public class Player : GameObject
         _position.X = startX;
         _position.Y = startY;
     }
+
+    private void Move()
+    {
+        if (Input.IsKey(ConsoleKey.UpArrow))
+        {
+            _position.Y = _position.Y - 1;
+        }
+        else if (Input.IsKey(ConsoleKey.DownArrow))
+        {
+            _position.Y = _position.Y + 1;
+        }
+        else if (Input.IsKey(ConsoleKey.LeftArrow))
+        {
+            _position.X = _position.X - 1;
+        }
+        else if (Input.IsKey(ConsoleKey.RightArrow))
+        {
+            _position.X = _position.X + 1;
+        }
+    }
+
     public override void Draw(ScreenBuffer buffer)
     {
-        throw new NotImplementedException();
+        buffer.SetCell(Position.X, Position.Y, '@', ConsoleColor.Green);
     }
 
     public override void Update(float deltaTime)
     {
-        throw new NotImplementedException();
+        Move();
     }
 }
